96cff81c1cc316fc42b90ae1fed9b28b4016ecab
Anton <antoniys007@gmail.com>
2023-04-17 14:42:40 +0700

    GAS

---
+++ Source/GAS/Private/BaseAttributeSet.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseAttributeSet.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+#include "BaseCharacter.h"
+
+UBaseAttributeSet::UBaseAttributeSet() {}
+
+void UBaseAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) 
+{
+    Super::PostGameplayEffectExecute(Data);
+
+    if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+    {
+        SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+    }
+    if (Data.EvaluatedData.Attribute == GetStaminaAttribute())
+    {
+        SetStamina(FMath::Clamp(GetStamina(), 0.0f, GetMaxStamina()));
+    }
+}
---
+++ Source/GAS/Private/BaseCharacter.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseCharacter.h"
+#include "BaseAttributeSet.h"
+
+// Sets default values
+ABaseCharacter::ABaseCharacter()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComp");
+}
+
+// Called when the game starts or when spawned
+void ABaseCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    if (AbilitySystemComp)
+    {
+        BaseAttributeSetComp = AbilitySystemComp->GetSet<UBaseAttributeSet>();
+
+        AbilitySystemComp->GetGameplayAttributeValueChangeDelegate(BaseAttributeSetComp->GetHealthAttribute())
+            .AddUObject(this, &ABaseCharacter::OnHealthChagedNative);
+        AbilitySystemComp->GetGameplayAttributeValueChangeDelegate(BaseAttributeSetComp->GetStaminaAttribute())
+            .AddUObject(this, &ABaseCharacter::OnStaminaChagedNative);
+    }
+}
+
+// Called every frame
+void ABaseCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABaseCharacter::InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel)
+{
+    if (AbilitySystemComp)
+    {
+        if (HasAuthority() && AbilityToGet)
+        {
+            AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(AbilityToGet, AbilityLevel, 0));
+        }
+        AbilitySystemComp->InitAbilityActorInfo(this, this);
+    }
+}
+
+UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
+{
+    return AbilitySystemComp;
+}
+
+void ABaseCharacter::GetHealthValues(float& Health, float& MaxHealth) 
+{
+    if (BaseAttributeSetComp)
+    {
+        Health = BaseAttributeSetComp->GetHealth();
+        MaxHealth = BaseAttributeSetComp->GetMaxHealth();
+    }
+}
+
+void ABaseCharacter::GetStaminaValues(float& Stamina, float& MaxStamina)
+{
+    if (BaseAttributeSetComp)
+    {
+        Stamina = BaseAttributeSetComp->GetStamina();
+        MaxStamina = BaseAttributeSetComp->GetMaxStamina();
+    }
+}
+
+void ABaseCharacter::OnHealthChagedNative(const FOnAttributeChangeData& Data)
+{
+    OnHealthChanged(Data.OldValue, Data.NewValue);
+}
+
+void ABaseCharacter::OnStaminaChagedNative(const FOnAttributeChangeData& Data)
+{
+    OnStaminaChanged(Data.OldValue, Data.NewValue);
+}
